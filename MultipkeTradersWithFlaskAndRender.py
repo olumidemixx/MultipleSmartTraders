fd8c446810b90bb3ea2e2c48e8fb4d2a42c76f66
olumidemixx <OLUMIDEMIXX@GMAIL.COM>
2025-01-13 08:31:04 +0100

    Update MultipkeTradersWithFlaskAndRender.py

--- MultipkeTradersWithFlaskAndRender.py
+++ MultipkeTradersWithFlaskAndRender.py
@@ -484,7 +484,7 @@ async def main():
     application.add_handler(CommandHandler("stop", stop))
 
     PORT = int(os.environ.get('PORT', '8080'))
-    WEBHOOK_URL = "https://multiplesmarttraders-pyh7.onrender.com/telegram"
+    WEBHOOK_URL = "https://multiplesmarttraders-2b9t.onrender.com/telegram"
     await application.bot.set_webhook(url=WEBHOOK_URL)
 
     app = Flask(__name__)
